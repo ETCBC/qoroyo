90d77b9e0d819b4a6a0c751e46130d8e41d5668c
Yusuf Celik <yusufcelik@icloud.com>
2025-01-22 21:05:44 +0100

    update

---
+++ static/js/289.6b6409e7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkqoroyo=self.webpackChunkqoroyo||[]).push([[289],{3289:function(e){e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.6b6409e7.chunk.js.map
